faea4190edb2e9c6c33e86959116f3c727b2e7ed
Ryv3x <112003314+Ryv3x@users.noreply.github.com>
2025-07-04 12:06:20 +0530

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Anmo9776/Obby-Tool-V1/refs/heads/main/obf_59Bg5SO3olTM7bPZf2a7bNvIZhva1cFf4wn68om9o0J5v1RTofOJC9dKG1Mk8e9u.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryv3x/Obby-Tool-V1/refs/heads/main/obf_59Bg5SO3olTM7bPZf2a7bNvIZhva1cFf4wn68om9o0J5v1RTofOJC9dKG1Mk8e9u.lua"))()
